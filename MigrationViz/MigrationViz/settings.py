4997d621472f4bc015491d52027f0f6e3717e5ea
willy <wilg1569@gmail.com>
2020-03-31 17:21:44 +0200

    Allowed hosts correction - put back into settings

--- MigrationViz/MigrationViz/settings.py
+++ MigrationViz/MigrationViz/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+ALLOWED_HOSTS = [.localhost, '127.0.0.1', 'france-work-migrations.herokuapp.com']
 
 
 # Application definition
